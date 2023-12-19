5e6655fa17055ce58ba8ca73ff9ef6c430c47d55
Andy <70863174+alin5201314@users.noreply.github.com>
2023-12-19 16:42:54 -0500

    Add UI components and assets for game
    - Update index.html with game components.
    - Modify js-file.js to enhance game logic and interaction.
    - Add new image assets for rock, paper, and scissors.
    - Create style.css for styling game elements.
    
    This update includes the basic UI setup for the Rock-Paper-Scissors game, including images for each choice and initial CSS for layout and design.

--- js-file.js
+++ js-file.js
@@ -1,48 +1,105 @@
+// Game Logic Functions
 function getComputerChoice() {
-    computerChoices = ["rock","paper","scissors"]    
-    return computerChoices[(Math.floor(Math.random() * computerChoices.length))]
-} 
+    const computerChoices = ["rock", "paper", "scissors"];
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("It's a tie! Replaying the round...");
-        computerSelection = getComputerChoice();
-        return playRound(playerSelection, computerSelection);
+        return {
+            message: "It's a tie! Try again...",
+            win: null,
+            tie: true
+        };
     }
     switch (playerSelection) {
-    case "rock":
-        return {
-                message: (computerSelection === "scissors") ? "You win! Rock beats Scissors" : "You lose! Paper beats Rock",
+        case "rock":
+            return {
+                message: computerSelection === "scissors" ? "You win! Rock beats Scissors" : "You lose! Paper beats Rock",
                 win: computerSelection === "scissors"
             };
         case "paper":
-        return {message: (computerSelection === "rock") ? "You win! Paper beats Rock" : "You lose! Scissors beats Paper",
-        win:computerSelection === "rock"
+            return {
+                message: computerSelection === "rock" ? "You win! Paper beats Rock" : "You lose! Scissors beats Paper",
+                win: computerSelection === "rock"
             };
         case "scissors":
-            return {message: (computerSelection === "paper") ? "You win! Scissors beats Paper" : "You lose! Rock beats Scissors",
-            win:computerSelection === "paper"
-        };
-
-    default:
-        return "Invalid player selection.";
-    }
-} 
-
-function game() {
-    let playerSelection = prompt("Rock,Paper, or scissors")
-    playerSelection.toLocaleLowerCase()
-    let computerSelection = getComputerChoice();
-    let result = playRound(playerSelection, computerSelection)
-    console.log(result.message);
-    console.log("Player:"+playerSelection);
-    console.log("Computer:"+computerSelection);
-    return result.win
-}
-let wins = 0
-for (let index = 0; index < 5; index++) {
-    if (game()) {
-        wins++;
+            return {
+                message: computerSelection === "paper" ? "You win! Scissors beats Paper" : "You lose! Rock beats Scissors",
+                win: computerSelection === "paper"
+            };
+        default:
+            return "Invalid player selection.";
     }
 }
-console.log("Total wins: " + wins);
 
+// DOM Manipulation Functions
+function updateScores(result) {
+    if (result.win) {
+        playerScore++;
+    } else {
+        computerScore++;
+    }
+    updateScoreDisplay();
+}
+
+function updateScoreDisplay() {
+    playerScoreElement.textContent = `Player: ${playerScore}`;
+    computerScoreElement.textContent = `Computer: ${computerScore}`;
+}
+
+function displayResult(resultMessage) {
+    message.textContent = resultMessage;
+    message.style.textAlign = "center";
+    message.style.fontSize = "3rem";
+    messageParent.appendChild(message);
+}
+
+function endGame() {
+    let winner = playerScore === maxScore ? "Player" : "Computer";
+    message.textContent = `${winner} wins! Final Score - Player: ${playerScore}, Computer: ${computerScore}. Click here to restart.`;
+    message.style.textAlign = "center";
+    message.style.fontSize = "3rem";
+    messageParent.appendChild(message);
+    images.forEach(img => img.style.pointerEvents = 'none');
+    messageParent.style.cursor = 'pointer';
+    messageParent.addEventListener('click', restartGame);
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScoreDisplay();
+    images.forEach(img => img.style.pointerEvents = 'auto');
+    messageParent.style.cursor = 'default';
+    messageParent.removeEventListener('click', restartGame);
+    message.textContent = "Choose your move!";
+}
+
+// Event Listeners
+const playerScoreElement = document.querySelector('.pScore');
+const computerScoreElement = document.querySelector('.cScore');
+const messageParent = document.querySelector('div .goal');
+const message = document.createElement('div');
+const images = document.querySelectorAll('img');
+
+let playerScore = 0;
+let computerScore = 0;
+const maxScore = 5;
+
+images.forEach(button => {
+    button.addEventListener('click', () => {
+        const playerSelection = button.id;
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+
+        if (!result.tie) {
+            updateScores(result);
+            if (playerScore === maxScore || computerScore === maxScore) {
+                endGame();
+                return;
+            }
+        }
+        displayResult(result.message);
+    });
+});
